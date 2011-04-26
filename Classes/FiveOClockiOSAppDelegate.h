9a180e94c0411ef1982e991e580fa5d388414f47
M. Adam Kendall <linuxbozo@gmail.com>
2011-04-26 16:22:04 -0400

    Setup the basics for iOS development

---
+++ Classes/FiveOClockiOSAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  FiveOClockiOSAppDelegate.h
+//  FiveOClockiOS
+//
+//  Created by akendall on 4/26/11.
+//  Copyright devis 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface FiveOClockiOSAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FiveOClockiOS
+//
+//  Created by akendall on 4/26/11.
+//  Copyright devis 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"FiveOClockiOSAppDelegate");
+    [pool release];
+    return retVal;
+}
